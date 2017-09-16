b23f496aa94a994b682f6672dfd21d2ab4ef07b3
savleen28 <savleenbedi90@gmail.com>
2017-09-16 12:44:27 +0530

    adding TestClass2 changes after merge conflict resolution

--- src/com/eclipse/test/TestClass2.java
+++ src/com/eclipse/test/TestClass2.java
@@ -4,6 +4,15 @@ public class TestClass2 {
 	
 	TestClass2(){
 		System.out.println("TestClass2 Constructor called");
+
+		System.out.println("Added TestClass2 Constructor");
+	}
+	public static void main(String[] args) {
+		System.out.println("This is the  second test class ");
+		System.out.println("Before calling the constructor for class two");
+		new TestClass2();
+		System.out.println("After calling the constructor for class two");
+
 	}
 
 }
