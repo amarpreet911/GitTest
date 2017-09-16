998faa82bd0aaec59530a6576aea8033bc449d12
amarpreet911 <Donaurkaun@911>
2017-09-16 01:33:48 -0400

    Added costructor for TestClass2

--- src/com/eclipse/test/TestClass2.java
+++ src/com/eclipse/test/TestClass2.java
@@ -1,10 +1,12 @@
 package com.eclipse.test;
 
 public class TestClass2 {
+	
+	TestClass2(){
+		System.out.println("Added TestClass2 Constructor");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is the  second test class ");
+		new TestClass2();
 	}
-
 }
-
-
