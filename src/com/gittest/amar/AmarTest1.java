2dbdb6e7f3e3cdc2219fd32b31cbba2999a2f2bd
amarpreet911 <Donaurkaun@911>
2017-09-17 01:28:57 -0400

    modified branch amar

--- src/com/gittest/amar/AmarTest1.java
+++ src/com/gittest/amar/AmarTest1.java
@@ -3,6 +3,7 @@ package com.gittest.amar;
 public class AmarTest1 {
 	public static void main(String args[]) {
 		new AmarTest2();
+		System.out.println("Added by amar second commit in branch amar");
 	}
 
 }
