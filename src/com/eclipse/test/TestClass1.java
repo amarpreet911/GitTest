44e3afda994b11b893fa3504f5fccdb1e50dda0f
savleen28 <savleenbedi90@gmail.com>
2017-09-17 08:59:08 +0530

    committing changes made by savleen
    Change-Id: I537131a080667a4b59d3ad53c3bb7125320dde84

--- src/com/eclipse/test/TestClass1.java
+++ src/com/eclipse/test/TestClass1.java
@@ -2,7 +2,7 @@ package com.eclipse.test;
 
 public class TestClass1 {
 
-	public TestClass1(){
+	public TestClass1() {
 		System.out.println("This is a constructor of TestClass1");
 		foo();
 		foo1();
@@ -13,12 +13,20 @@ public class TestClass1 {
 		System.out.println("Amar test o in foo::::2");
 		System.out.println("Amar test o in foo::::3");
 		System.out.println("Amar test o in foo::::4");
+		System.out.println("This is the first function of TestClass1 --Savleen");
+		System.out.println("This is the first function of TestClass2 --Savleen");
+		System.out.println("This is the first function of TestClass3 --Savleen");
+		System.out.println("This is the first function of TestClass4 --Savleen");
 	}
-	
+
 	public void foo1() {
 		System.out.println("Amar test o in foo1::::1");
 		System.out.println("Amar test o in foo1::::2");
 		System.out.println("Amar test o in foo1::::3");
 		System.out.println("Amar test o in foo1::::4");
+		System.out.println("This is the second function of TestClass5 --Savleen");
+		System.out.println("This is the second function of TestClass6 --Savleen");
+		System.out.println("This is the second function of TestClass7 --Savleen");
+		System.out.println("This is the second function of TestClass8 --Savleen");
 	}
 }
