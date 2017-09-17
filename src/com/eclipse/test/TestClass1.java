72d02b059ce1b6654948d8a4c3b400bdbddba2c1
amarpreet911 <Donaurkaun@911>
2017-09-16 23:07:18 -0400

    TestClass1 modified
    Change-Id: Iaa665aabf0d740befd061734c297979bb8334f8e

--- src/com/eclipse/test/TestClass1.java
+++ src/com/eclipse/test/TestClass1.java
@@ -4,5 +4,21 @@ public class TestClass1 {
 
 	public TestClass1(){
 		System.out.println("This is a constructor of TestClass1");
+		foo();
+		foo1();
+	}
+
+	public void foo() {
+		System.out.println("Amar test o in foo::::1");
+		System.out.println("Amar test o in foo::::2");
+		System.out.println("Amar test o in foo::::3");
+		System.out.println("Amar test o in foo::::4");
+	}
+	
+	public void foo1() {
+		System.out.println("Amar test o in foo1::::1");
+		System.out.println("Amar test o in foo1::::2");
+		System.out.println("Amar test o in foo1::::3");
+		System.out.println("Amar test o in foo1::::4");
 	}
 }
