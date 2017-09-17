2d81ab2a2504a9ddd68ecf51b1b0798e09f6f024
amarpreet911 <Donaurkaun@911>
2017-09-17 00:59:41 -0400

    New branch amar first commit

---
+++ src/com/gittest/amar/AmarTest1.java
@@ -0,0 +1,8 @@
+package com.gittest.amar;
+
+public class AmarTest1 {
+	public static void main(String args[]) {
+		new AmarTest2();
+	}
+
+}
---
+++ src/com/gittest/amar/AmarTest2.java
@@ -0,0 +1,8 @@
+package com.gittest.amar;
+
+public class AmarTest2 {
+
+	public AmarTest2() {
+		System.out.println("In constructor for AmarTest2 initiated from AmarTest1");
+	}
+}
