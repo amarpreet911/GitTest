ee546efbffafec88d047cb477d2c531361b34c61
savleen28 <savleenbedi90@gmail.com>
2017-09-17 11:07:38 +0530

    commiting conflict resloved AmarTest1 branch amar
    Change-Id: I0c66d249cbdfe6877b66d42e31b6020d94765575

--- src/com/gittest/amar/AmarTest1.java
+++ src/com/gittest/amar/AmarTest1.java
@@ -1,9 +1,11 @@
+
 package com.gittest.amar;
 
 public class AmarTest1 {
 	public static void main(String args[]) {
 		new AmarTest2();
 		System.out.println("Added by amar second commit in branch amar");
+		System.out.println("sysout added by savleen in amar branch");
 	}
-
 }
+
